f8155a0e32c47751b5caeaade671f3859433d05c
Sneezles <seba_skrbi@hotmail.com>
2021-11-29 15:50:39 +0100

    Should be working now, added process.env.PORT

--- server/server.js
+++ server/server.js
@@ -150,7 +150,7 @@ server.on('error', (err) => {
   console.error(err);
 });
 
-server.listen(8080, () => {
+server.listen(process.env.PORT || 8080, () => {
   console.log('server is ready');
 });
 
